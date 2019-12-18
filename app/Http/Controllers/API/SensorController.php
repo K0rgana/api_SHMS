92daa1f425eeb600cb98ccbfb0a30aa1b420631e
siilvatamires <tamiresmaria561@gmail.com>
2019-12-17 23:35:41 -0300

    corrigindo validações do sensor

--- app/Http/Controllers/API/SensorController.php
+++ app/Http/Controllers/API/SensorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use App\Sensor;
 use App\API\ApiError;
@@ -24,8 +25,13 @@ class SensorController extends Controller
      */
     public function index()
     {
-        $data = ['data' => $this->sensor::all()];
-        return response() -> json($data);
+        if(sizeof($this->sensor->all()) <= 0 ){
+            return response()->json(['data' => ['msg' => 'nenhum sensor cadastrado']], 404);
+        }
+        else{
+            $data = ['data' => $this->sensor::all()];
+            return response() -> json($data);
+        }
     }
 
     /**
@@ -37,11 +43,11 @@ class SensorController extends Controller
     public function store(Request $request)
     {   
         $validator = Validator::make($request->all(), [
-            'nome' => 'required|string|min:2|max:50',
+            'nome' => 'required|string|min:2|max:100',
             'tipo' => 'in:temperatura, luminosidade, presenca, magnetico|required|min:8|max:12'
         ]);
         
-        if($validator->errors()){
+        if(sizeof($validator->errors()) > 0 ){
             return response()->json($validator->errors(), 404);
         }
 
@@ -55,7 +61,9 @@ class SensorController extends Controller
 			if(config('app.debug')) {
 				return response()->json(ApiError::errorMessage($e->getMessage(), 500), 500);
 			}
-			return response()->json(ApiError::errorMessage('erro ao realizar operação de salvar', 500),  500);
+			if(is_null($sensors)) {
+                return response()->json(['data'=>['msg'=>'sensor não encontrado!']], 404);
+            }
 		}
     }
 
@@ -68,10 +76,10 @@ class SensorController extends Controller
     public function show(Sensor $id)
     {
         $sensor = $this->sensor::with('sensor')->find($id);
-    	if(!$sensor) {
+        if(is_null($sensor)) {
             return response()->json(ApiError::errorMessage('sensor não encontrado.', 404), 404);
         }
-        
+
         $data = ['data' => $sensor];
         return response() -> json($data);
     }
@@ -85,24 +93,31 @@ class SensorController extends Controller
      */
     public function update(Request $request, $id)
     {       
-      $validator = Validator::make($id, $request->all(),[
-            'id' => 'required|number',
-            'nome' => 'required|string|min:2|max:50',
-            'tipo' => 'in:temperatura, luminosidade, presença, magnético|required|min:8|max:10'
+        $validator = Validator::make($id, $request->all(),[
+            'nome' => 'required|string|min:2|max:100',
+            'tipo' => 'in:temperatura, luminosidade, presenca, magnetico|required|min:8|max:12'
             ]);
 
+        if(sizeof($validator->errors()) > 0 ){
+            return response()->json($validator->errors(), 404);
+        }
         try {
             $sensordata = $request->all();
             $sensor = $this->sensor->find($id);
-            $sensor->update($sensordata);
 
-			$return = ['data' => ['msg' => 'sensor atualizado com sucesso.']];
-			return response()->json($return, 201);
+            if($sensor){
+                $sensor->update($sensordata);
+				$return = ['data' => ['msg' => 'sensor atualizado com sucesso.']];
+				return response()->json($return, 201);
+			}
+			else{
+				return response()->json(['data' => ['este sensor nao existe e nao pode ser atualizado']], 404);
+			}
+
 		} catch (\Exception $e) {
 			if(config('app.debug')) {
 				return response()->json(ApiError::errorMessage($e->getMessage(), 500),  500);
 			}
-			return response()->json(ApiError::errorMessage('erro ao realizar operação de atualizar', 500), 500);
 		}
     }
 
@@ -114,11 +129,15 @@ class SensorController extends Controller
      */
     public function destroy(Sensor $id)
     {
+        $sensor = Sensor::find($id);
         try {
-            
-            $id->delete();
-            return response()-> json(['data'=> ['msg'=> 'sensor: '.$id->nome. ' deletado com sucesso.']],200);
-
+            if($sensor){
+				$sensor->delete();
+				return response()->json(['data' => ['msg' => 'sensor: '.$id->nome. ' deletado com sucesso.']], 200);
+			}
+			else{
+				return response()->json(['data' => ['msg' => 'este sensor nao existe e nao pode ser deletado']], 500);
+			}
 		}catch (\Exception $e) {
 			if(config('app.debug')) {
 				return response()->json(ApiError::errorMessage($e->getMessage(), 500),  500);
